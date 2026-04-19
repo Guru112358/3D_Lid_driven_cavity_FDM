0ab2110b43051aafe5c80ffc748a2518e1b8d02c
Guruprasad S <61557979+Guru112358@users.noreply.github.com>
2026-04-19 16:44:45 +0530

    Update params.cpp

--- params.cpp
+++ params.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<cmath>
 #include<Eigen/Dense>
-#include  <eigen3/unsupported/Eigen/CXX11/Tensor>
+#include  <unsupported/Eigen/CXX11/Tensor>
 #include<fstream>
 #include<omp.h>
 
